17717a67de307204365b7ed966656962aaa7a2d5
Alban Hoxha <deepserket@gmail.com>
2018-07-25 09:02:40 +0200

    code refactoring

--- rockstarpy/command_line.py
+++ rockstarpy/command_line.py
@@ -3,19 +3,16 @@ from . import rockstar
 
 parser = argparse.ArgumentParser(description="Python transpiler for the esoteric language Rockstar")
 parser.add_argument('input', action='store', help='Input file (.rock|.rockstar|.lyrics)')
-parser.add_argument('--output', action='store', help='Output file (.py)')
+parser.add_argument('--output', action='store', help='Output file (.py)', default='output.py')
 
 def main():
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	try:
-	    rockstar_file = open(args.input, 'r')
-	    rockstar_code = rockstar_file.readlines()
-	    rockstar_file.close()
-	except FileNotFoundError:
-	    print('File not found.')
-
-	output_file = args.output if args.output else 'output.py'
-	py_rockstar = open(output_file, 'w')
-	rockstar.convert_code(rockstar_code, py_rockstar)
-	py_rockstar.close()
+    try:
+        with open(args.input, 'r') as rockstar_file:
+            rockstar_code = rockstar_file.readlines()
+    except FileNotFoundError:
+        print('File not found.')
+    else:
+        with open(args.output, 'w') as py_rockstar:
+            rockstar.convert_code(rockstar_code, py_rockstar)
